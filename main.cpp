b029bbccfc3d2dd082c940d4b439478d612214c8
Adam Kalina <adam.kalina89@gmail.com>
2022-09-26 22:18:14 +0200

    - added option for using just one argument

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     QCommandLineParser parser;
     parser.setApplicationDescription(
-"                                                :.\n"
+                "                                                :.\n"
 "                                              ~?7.\n"
 "                                           .~J5?:\n"
 "                 .::::.                  .~J55?.\n"
@@ -34,11 +34,12 @@ int main(int argc, char *argv[])
 "        :::^!?JJ?!~^^~~!!7??7!~~!77!!!!777?????7?JYYJ?777\n"
 "            !!!!!777!!7?JJ??7!!~~~~~~!!77????7!~^:....\n"
 "\n"
-"Cuculus is .SIG (Brainlab) to .EDF converter based of EDFlib by Teuniz and sigtoedf by Frederik-D-Weber. Bashed together by Adam Kalina 2022, Motol University Hospital");
+"Cuculus is .SIG (Brainlab) to .EDF converter based of EDFlib by Teuniz and sigtoedf by Frederik-D-Weber.\nBashed together by Adam Kalina 2022, Motol University Hospital");
     parser.addHelpOption();
     parser.addVersionOption();
+    parser.addOption(QCommandLineOption("a", QCoreApplication::translate("main", "Anonymize output")));
     parser.addPositionalArgument("input", QCoreApplication::translate("main", "Source *.SIG file"));
-    parser.addPositionalArgument("output", QCoreApplication::translate("main", "Target *.EDF file"));
+    parser.addPositionalArgument("output", QCoreApplication::translate("main", "Target *.EDF file - optional"));
 
     // Process the actual command line arguments given by the user
     parser.process(a);
@@ -46,12 +47,11 @@ int main(int argc, char *argv[])
     const QStringList args = parser.positionalArguments();
     // source is args.at(0), destination is args.at(1)
 
-    if(args.size() == 2){ //for now the app will require both arguments - TO DO - option for one argument, where the edf file is created in the same folder
-        //qDebug() << "Here!";
-        qDebug() << args.at(0); // TO DO - must be SIG file
-        qDebug() << args.at(1); // TO DO - must be EDF file
+    if(args.size() == 2){
+        qDebug() << args.at(0);
+        qDebug() << args.at(1);
 
-        //TO DO - must be SIG file
+        //must be SIG file
         QFileInfo infoSig(args.at(0));
 
         if(infoSig.completeSuffix() == "SIG" && infoSig.exists()){
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        // TO DO - must be EDF file
+        //must be EDF file
         QFileInfo infoEdf(args.at(1));
 
 
@@ -78,6 +78,23 @@ int main(int argc, char *argv[])
             SignalFile signal = read_signal_file(infoSig); //TO DO - check if the file is oky etc.
             write_edf_file(&signal,infoEdf);
         }
+    }
+
+    if(args.size() == 1){ // when only one argument is provided
+        //must be SIG file
+        QFileInfo infoSig(args.at(0));
+
+        if(infoSig.completeSuffix() == "SIG" && infoSig.exists()){
+            qDebug() << "Only path to input was provided, exporting to the same folder";
+            SignalFile signal = read_signal_file(infoSig); //TO DO - check if the file is oky etc.
+            QFileInfo infoEdf(infoSig.path() + "/" + infoSig.completeBaseName() + ".EDF");
+            write_edf_file(&signal,infoEdf);
+        }
+        else{
+            qDebug() << "The input file does not exist or it is not a .SIG file - exiting";
+            return 1;
+        }
+
     }
 
     return a.exec();
